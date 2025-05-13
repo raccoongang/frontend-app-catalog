44ee6c70b41a4d17233cdcc1808981b0fd2b1bb0
vladislavkeblysh <vladislav.keblysh@raccoongang.com>
2025-05-13 19:19:52 +0300

    feat: refactor after review

--- src/setupTest.js
+++ src/setupTest.js
@@ -2,7 +2,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
-import { AppProvider } from '@edx/frontend-platform/react';
 import { render as rtlRender } from '@testing-library/react';
 import PropTypes from 'prop-types';
 
@@ -10,9 +9,7 @@ function render(ui) {
   const Wrapper = ({ children }) => (
     // eslint-disable-next-line react/jsx-filename-extension
     <IntlProvider locale="en">
-      <AppProvider>
-        {children}
-      </AppProvider>
+      {children}
     </IntlProvider>
   );
 
